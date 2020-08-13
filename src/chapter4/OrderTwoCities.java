ba5b19514d35b4a700b927a065a85821d144d9ef
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-13 02:05:40 -0400

    example 4.2

---
+++ src/chapter4/OrderTwoCities.java
@@ -0,0 +1,22 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class OrderTwoCities {
+	
+	public static void main(String[] args) 
+	{
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Enter two cities : ");
+	
+	String city1 = input.nextLine();
+	String city2 = input.nextLine();
+	
+	if (city1.compareTo(city2) < 0) System.out.println(city1 + city2);
+	else System.out.println(city2 + city1);
+		
+		
+	
+	}
+}
--- src/chapter4/ex15.java
+++ src/chapter4/ex15.java
@@ -5,7 +5,7 @@ public class ex15 {
 	
 	public static void main(String[] args)
 	{
-		
+		  
 	}
 }
 
