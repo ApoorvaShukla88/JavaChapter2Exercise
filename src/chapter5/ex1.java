138fac3fd3dd3b82691e8109d9cc9e2f9fdd1e93
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-18 13:22:57 -0400

    ex1 changed

--- src/chapter5/ex1.java
+++ src/chapter5/ex1.java
@@ -4,36 +4,38 @@ import java.util.Scanner;
 
 public class ex1 {
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Enter an integer (the input ends if it ends with 0) : ");
 		int num = input.nextInt();
-		
+
 		double sum = 0.0;
 		int positivecount = 0;
 		int negativecount = 0;
-		
+
 		while(num != 0) {
 			sum += num;
 			if(num > 0 ) {
 
 				positivecount++;
-				System.out.println("The number of positives is : " + positivecount);
+//				System.out.println("The number of positives is : " + positivecount);
 			}else if (num < 0){
 
 				negativecount++;
-				System.out.println("The number of negatives is : " + negativecount);
+//				System.out.println("The number of negatives is : " + negativecount);
 			}
+			System.out.println("The number of positives is : " + positivecount);
+			System.out.println("The number of negatives is : " + negativecount);
 			System.out.println("Total number of entry is :" + (positivecount + negativecount));
-			System.out.println("Enter an integer (the input ends if it ends with 0) : ");
+//			System.out.println("Enter an integer (the input ends if it ends with 0) : ");
 			num = input.nextInt();
 
 		}
 
 		System.out.println("The average is : " + sum / (positivecount + negativecount));
 	}
-		
-		
-		
-	}
+
+
+
+}
