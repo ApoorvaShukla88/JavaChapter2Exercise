428916746e0c9090f0f320860872ebcc126d5c26
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-18 15:22:11 -0400

    Nested loops paterns

--- src/chapter5/Nestedloop.java
+++ src/chapter5/Nestedloop.java
@@ -4,26 +4,58 @@ import java.util.Scanner;
 
 public class Nestedloop {
 	public static void main(String[] args) {
+		//		System.out.println("Patern A");	
+		//		for( int i  = 1; i <= 6 ; i++) {
+		//			for(int j = 1; j <= i ; j++) {
+		//				System.out.print(j);
+		//			}
+		//			System.out.println();
+		//		}
+//		System.out.println("Pattern B");
+//		for( int i = 6; i >= 1 ; i--) {
+//			for(int j = 1 ; j <= i ; j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println();
+//		123456
+//		12345
+//		1234
+//		123
+//		12
+//		1
 		
-		for(int i = 6; i <= 1  ; i--) {
-			for(int j = 1; j <= i ; j++) 
-			{
-			
-				System.out.print(j);
+//		System.out.println("Pattern C");
+//		
+//		for(int i = 0; i <= 6; i++) {
+//			for(int j = 6 ; j >= 1 ; j--) {
+//				System.out.print((i<j) ? " " : j);
+//			}
+//		System.out.println();
+//			
+//		}
+//		System.out.println();
+//		
+//	      
+//	     1
+//	    21
+//	   321
+//	  4321
+//	 54321
+//	654321
+		
+		System.out.println("Pattern D");
+		for(int i = 1; i <= 6; i++) {
+			for(int j = 1 ; j <= 6 ; j++) {
+				System.out.print((i>j) ? " " : j);
 			}
-		   System.out.println();
+		System.out.println();
+			
 		}
+		System.out.println();
 		
 		
 		
 		
-			}
-		
-			
-		
-		
-		
-		
-		
-		
-	}
+	}}
