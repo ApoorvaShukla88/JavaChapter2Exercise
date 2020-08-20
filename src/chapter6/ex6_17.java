1b9ecf3fbd5ee67b00cbf7c97e3689ac2d9db6bc
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-20 12:40:17 -0400

    exercise 6.17

--- src/chapter6/ex6_17.java
+++ src/chapter6/ex6_17.java
@@ -1,8 +1,28 @@
 package chapter6;
 
+import java.util.Scanner;
+
 public class ex6_17 {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number for matrix : ");
+		int num= input.nextInt();
+		printMatrix(num);
 		
+		
+	}
+	
+	public static void printMatrix(int n) {
+
+		for (int row = 1; row <=n ; row++) {
+			for (int col = 1; col <= n; col++) {
+
+				System.out.print((int)(Math.random() * 2));
+//				System.out.print();
+			}
+			
+		System.out.println();
+		}
 	}
 	
 }
