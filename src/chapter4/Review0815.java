c8795e5966c112efc61bf1694ada4b8ad84aa77c
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-20 09:19:25 -0400

    changed files

---
+++ src/chapter4/Review0815.java
@@ -0,0 +1,98 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class Review0815 {
+	
+	public static void main(String[] args)
+	{
+//	int n = 10;
+//	int count = 0;
+//	
+//	for(int i = 1; i < n; i++) {
+//		if(i%3 ==0 || i%5 == 0) {
+//		count++;
+////			System.out.println(i);	
+//		}
+////		System.out.println("Answers is : " + count);
+//	}
+//	System.out.println("Answers is : " + count);
+//	
+	
+//	=====================================
+//Scanner input = new Scanner(System.in);
+//	System.out.println("Enter your name : ");
+//	String name = input.nextLine();
+//	String c = Character.toLowerCase(name);
+//	int counter = 0;
+//	for(int i =0 ; i < name.length(); i++) {
+//		if(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+//			counter++;
+//		}
+//	System.out.println(counter);
+//	}
+//	
+//	
+//	
+//	
+	
+// ================================================
+//	
+//	Scanner input = new Scanner(System.in);
+//	System.out.println("Enter your name : ");
+//	String username = input.nextLine();
+//	
+//	int commaIndex = username.indexOf(',');
+//	System.out.println(commaIndex);
+//	
+//	String lastname = username.substring(0, commaIndex);
+//	String firstname = username.substring(commaIndex+ 2, username.length());
+//	System.out.println(firstname + " " +lastname);
+//	
+	
+// =========================================
+//	find the first n prime number
+//	
+//	boolean isPrime = true;
+	int counter = 0;
+//	int i =50;
+////	System.out.println("Enter the how many prime number you want from 1 :");
+//	while(counter < 5) {
+////		isPrime = true;
+//		for( int j =2; j < i; j++) {
+//			if(i % j == 0) {
+//				isPrime = false;
+//			}
+//				System.out.println(j);
+//			}
+//			
+//		}
+//		
+//		i++;	
+		
+		
+		// first 10 prime number
+		
+		while(counter < 10) {
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	}
