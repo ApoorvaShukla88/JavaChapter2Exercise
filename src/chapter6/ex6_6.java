2144215c5e284040e359cba8f075f248b1b777aa
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-20 09:15:36 -0400

    ex6.6 done

--- src/chapter6/ex6_6.java
+++ src/chapter6/ex6_6.java
@@ -2,6 +2,18 @@ package chapter6;
 
 public class ex6_6 {
 	public static void main(String[] args) {
+		display(6);
+		
+	}
+	
+	public static void display(int n) {
+		for(int i = 1; i <= n ; i++) {
+			for(int j = n; j >= 1; j--) {
+				System.out.print((i<j) ? " " : j);
+			}
+			System.out.println();
+		}
+		System.out.println();
 		
 	}
 	
