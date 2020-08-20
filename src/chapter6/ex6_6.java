c3b9f05f4c3506c77a6e2f064c438bf5892d0f2f
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-08-20 12:41:17 -0400

    exercise 6.17

--- src/chapter6/ex6_6.java
+++ src/chapter6/ex6_6.java
@@ -2,19 +2,28 @@ package chapter6;
 
 public class ex6_6 {
 	public static void main(String[] args) {
-		display(6);
+		displayPatterns(15);
 		
 	}
 	
-	public static void display(int n) {
-		for(int i = 1; i <= n ; i++) {
-			for(int j = n; j >= 1; j--) {
-				System.out.print((i<j) ? " " : j);
+	public static void displayPatterns(int n) {
+		for(int i = 1; i <= n; i++) {
+		for(int j = n ; j >=1  ; j--) {
+			if(i<j) {
+				System.out.print(" ");
+			}else {
+				if(i>10 && j > 10) System.out.print("" + j);
+				else System.out.print(j);
+				
 			}
-			System.out.println();
+					
+			
+//			System.out.print((i<j) ? " " : (j>10) ? " ": j-1);
 		}
 		System.out.println();
 		
 	}
+//	System.out.println();
+    } 
 	
 }
