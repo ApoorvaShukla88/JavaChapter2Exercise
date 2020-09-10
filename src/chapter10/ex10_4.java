44ee7068c2ccb9640890022abdfa05a8e28dbd21
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-09-10 18:07:29 -0400

    chapeter 12

---
+++ src/chapter10/ex10_4.java
@@ -0,0 +1,45 @@
+package chapter10;
+
+public class ex10_4{
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
+
+class MyPoint{
+	private static int x;
+	private static int y;
+	
+	MyPoint(){
+		this.x = 0;
+		this.y = 0;
+		
+	}
+	MyPoint(int x, int y){
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public static int getX() {
+		return x;
+	}
+	public static void setX(int x) {
+		MyPoint.x = x;
+	}
+	public static int getY() {
+		return y;
+	}
+	public static void setY(int y) {
+		MyPoint.y = y;
+	}
+	
+	public double distance(MyPoint m) {
+		
+		return double ;
+		
+	}
+	
+}
--- src/chapter12/ex12_15.java
+++ src/chapter12/ex12_15.java
@@ -1,17 +1,48 @@
 package chapter12;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class ex12_15{
 	public static void main(String[] agrs) throws Exception{
-			try {
-				Scanner input = new Scanner(new File("src/chapter12/Exercise12_15.txt"));
-				if()
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
+//				Scanner input = new Scanner(new File("src/chapter12/Exercise12_15.txt"));
+//			if(.exists()) {
+//				System.out.println("File already exists");
+//				System.exit(1);
+//			}
+		File file = new File("src/chapter12/Exercise12_15.txt");
+		try {
+			if(file.exists()) 
 		
+		System.out.println("File already exists");
+		System.exit(1);
+		}catch(Exception e) {
+			
+		}
+			PrintWriter output = new PrintWriter(file);
+			for(int i = 0; i < 100 ; i++)
+			{
+			output.print((int)Math.random() * 1000);
+			output.print(" ");
+			}
+//			output.close();
+			
+			Scanner input = new Scanner(file);
+			while(input.hasNext()) {
+				int numbers = input.nextInt();
+				int[] myArray = 
+				
+				
+				
+				
+				
+				
+			}
 	}
+	
+	
+	
+	
 }
