2b89e86296d390734c71f2ca708c34a20a919da6
ApoorvaShukla88 <Apoorvashukla88@gmail.com>
2020-09-12 14:04:43 -0400

    added hangman

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,29 @@
+package Hangman;
+
+
+//-ALLOWED_WRONG_GUESSES:int = 6
+//+setHiddenMessage(hiddenMessage:String)
+//+guessLetter(letter:char)
+//+getRemainingGuesses()
+//+getMessage()
+
+public class Hangman{
+	public static int ALLOWED_WRONG_GUESSES  = 6;
+	
+	public String message;
+	
+	public Hangman() {
+		this.message = "";
+	}
+	
+	public void setHiddenMessage(String message) {
+		
+		
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ src/Hangman/HangmanTest.java
@@ -0,0 +1,12 @@
+package Hangman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HangmanTest {
+
+	@Test
+	public void 
+
+}
