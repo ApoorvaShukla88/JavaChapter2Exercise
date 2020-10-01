0988b3319f61cd602c4508f973e06b369f2c7ec5
Apoorvashukla88 <apoorvashukla88@gmail.com>
2020-09-30 22:28:22 -0400

    chap20

--- src/chapter20List/practise.java
+++ src/chapter20List/practise.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class practise {
-    static String pre = null;
+    static String pre = "";
     public static void main(String[] args){
         System.out.println(commonPreffix("mining", "timing"));
         int[] list = {1, 2, 3, 5, 6,7 ,8,10, 11, 34, 55};
-        System.out.println(Arrays.toString(shiftElements(list)));
-
+//        System.out.println(Arrays.toString(shiftElements(list)));
+        System.out.println(Arrays.toString(shiftElement(list)));
     }
 
     public static String commonPreffix(String s1, String s2) {
@@ -38,17 +38,16 @@ public class practise {
       return myList;
     }
 
-    public static int[] shiftElements(int[] myList){
-//    Scanner input = new Scanner(System.in);
-//    int n = input.nextInt();
-
+    public static int[] shiftElement(int[] myList){
 // Shifting in array rigth to left
-
-        int temp = myList[0];
-        for(int i = 1; i< myList.length; i++){
-            myList[i-1] = myList[i];
+        // int[] list = {1, 2, 3, 5, 6,7 ,8,10, 11, 34, 55};
+        System.out.println(Arrays.toString(myList));
+        int temp = myList[myList.length -1];
+//        myList[0] = temp;
+        for(int i = 1;i<myList.length-1; i++){
+            myList[i-1] = myList[i];// 1>0, 2>1, 3>2....
         }
-        myList[myList.length - 1] = temp;
+        myList[0] = temp;
         return myList;
     }
 
